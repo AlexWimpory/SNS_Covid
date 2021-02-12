efe6930c4f182b79902789bb270ec0cddce99082
AlexWimpory <zceewim@ucl.ac.uk>
2021-02-12 18:02:08 +0000

    Restructure

--- sns_covid/data_processing/data_pre_processor.py
+++ sns_covid/data_processing/data_pre_processor.py
@@ -1,6 +1,9 @@
 from sns_covid import config
 import pandas as pd
 import numpy as np
+from numpy import split, vsplit, array_split
+from numpy import array
+
 
 def process_date(df):
     anchor_date = pd.to_datetime(df[config.date_column_name].iloc[0])
@@ -30,34 +33,41 @@ def save_data():
     pass
 
 
-def split_data(df):
-    n = len(df)
-    train_df = df[0:int(n * 0.7)]
-    val_df = df[int(n * 0.7):int(n * 0.9)]
-    test_df = df[int(n * 0.9):]
-    return train_df, val_df, test_df
+def split_data_train_test(data):
+    # data = data.to_numpy()
+    n = len(data)
+    train = data[0: int(n * 0.8)]
+    test = data[int(n * 0.8): n]
+    n = (len(train) // 7) * 7
+    train = train[len(train) - n:]
+    n = (len(test) // 7) * 7
+    test = test[0: n]
+    return train, test
 
 
-def normalise_data(train, val, test):
+def split_data_7_days(data):
+    n = (len(data) // 7) * len(data)
+    data = array_split(data, len(data) / 7, axis=0)
+    data_split = array(data)
+    return data_split
+
+
+def normalise_data(train, test):
     # TODO Use moving averages
     train_mean = train.mean()
     train_std = train.std()
     train = (train - train_mean) / train_std
-    val = (val - train_mean) / train_std
     test = (test - train_mean) / train_std
-    return train, val, test
+    return train, test
 
 
-def generate_train_val_test(df):
+def generate_train_test(df):
     processed_df = filter_data(df)
     processed_df = remove_nan(processed_df)
-    train_df, val_df, test_df = split_data(processed_df)
-    return normalise_data(train_df, val_df, test_df)
-
-
-def generate_train_val_test_2(df):
-    processed_df = filter_data(df)
-    processed_df = remove_nan(processed_df)
-    train_df, val_df, test_df = split_data(processed_df)
-    train_df, val_df, test_df = normalise_data(train_df, val_df, test_df)
-    return pd.concat([train_df, val_df, test_df])
+    train_df, test_df = split_data_train_test(processed_df)
+    train_df, test_df = normalise_data(train_df, test_df)
+    train_df = split_data_7_days(train_df)
+    test_df = split_data_7_days(test_df)
+    print(train_df.shape)
+    print(test_df.shape)
+    return train_df, test_df
--- sns_covid/main.py
+++ sns_covid/main.py
@@ -1,11 +1,7 @@
-from sns_covid import config
 from sns_covid.data_processing.data_loader import load_country
-from sns_covid.data_processing.data_pre_processor import generate_train_val_test
-from sns_covid.model.data_generator import DataGenerator
-from sns_covid.model.model_predictor import ModelPredictor
+from sns_covid.data_processing.data_pre_processor import generate_train_test
 from sns_covid.model.model_structures import *
-from sns_covid.model.model_trainer import CovidPredictionModel
-from sns_covid.visulisation.plotter import visualise_results
+
 
 
 # TODO Set frequency on dataframe to fill in any gaps (prob aren't any but good practice)(uses date column)
@@ -20,21 +16,8 @@ def main():
     # Load the data into a dataframe
     df = load_country(config.country_iso_code, download=False)
     # Generate the train, validation and test dataframes
-    train_df, val_df, test_df = generate_train_val_test(df)
-    # Turn the data into tensorflow readable data
-    data_generator = DataGenerator(train_df.columns)
-    train_ds, val_ds, test_ds = data_generator.make_datasets(train_df, val_df, test_df)
-    # Now for the model
-    model = CovidPredictionModel('dense', dense())
-    model.compile()
-    model.fit(train_ds, val_ds)
-    # Finally evaluate the model
-    print(model.test(val_ds))
-    print(model.test(test_ds))
-    # Do some predictions
-    predictor = ModelPredictor('dense')
-    test_predictions = predictor.predict(test_df)
-    visualise_results(test_predictions, test_df)
+    train_df, test_df = generate_train_test(df)
+    print('sss')
 
 
 if __name__ == '__main__':
